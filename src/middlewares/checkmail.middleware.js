1c7bb46d9e6515c638b80980ce8e9a073e9f1058
Ajay Sharma <sharmaa8203@gmail.com>
2025-11-19 17:40:58 +0530

    ak

--- src/middlewares/checkmail.middleware.js
+++ src/middlewares/checkmail.middleware.js
@@ -1,28 +1,20 @@
-import nodemailer from "nodemailer";
+import { Resend } from 'resend';
 import { asyncHandler } from "../../src/utils/asynchandler.js";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // SSL
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = asyncHandler(async (to, subject, text) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"Your App Name" <${process.env.EMAIL_USER}>`,
+    const data = await resend.emails.send({
+      from: 'Your App Name <onboarding@resend.dev>',
       to,
       subject,
       text,
     });
 
-    console.log("✅ Email sent:", info.response);
+    console.log("Email sent:", data);
   } catch (error) {
-    console.error("❌ Error sending email:", error);
-    throw new Error("Failed to send email");
+    console.error("Error sending email:", error.message);
+    throw error;
   }
 });
