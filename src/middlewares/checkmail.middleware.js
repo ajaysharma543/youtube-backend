7c3c8c2292ef05ec80d4e9eb6b8f0686b68e1107
Ajay Sharma <sharmaa8203@gmail.com>
2025-11-20 16:23:18 +0530

    email

--- src/middlewares/checkmail.middleware.js
+++ src/middlewares/checkmail.middleware.js
@@ -1,36 +1,24 @@
 import nodemailer from "nodemailer";
 import { asyncHandler } from "../../src/utils/asynchandler.js";
-
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
-    user: process.env.BREVO_USER,
-    pass: process.env.BREVO_SMTP_KEY,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
-  connectionTimeout: 60000,
-  greetingTimeout: 60000,
-  socketTimeout: 60000,
-  // IMPORTANT: force IPv4 for Render
-  family: 4
 });
+
 export const sendEmail = asyncHandler(async (to, subject, text) => {
-   try{
-     const info = await transporter.sendMail({
-      from: process.env.BREVO_USER,  
+  try {
+    const info = await transporter.sendMail({
+      from: `"Your App Name" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       text,
     });
-
-    console.log("Email sent:", info.messageId);
-   }
-   catch (error) {
-    console.error("Email error:", error);
+    console.log("✅ Email sent:", info.response);
+  } catch (error) {
+    console.error("❌ Error sending email:", error.message);
+    throw error;
   }
-}
-)
+});
